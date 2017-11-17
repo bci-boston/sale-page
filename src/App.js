a74e5bf68f68932540304a66e512385d36ec8d60
Binod Pant <binod@macbookpro.home>
2017-11-16 22:40:08 -0500

    update section of page instead of jus three spans, but use wordpress markup to avoid mis-sync of UI and still allow Sean to manage layout in Wordpress

--- src/App.js
+++ src/App.js
@@ -4,123 +4,6 @@ import CrowdSale from '../build/contracts/CrowdSale.json'
 
 import getWeb3 from './utils/getWeb3'
 
-class TokensForSale extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            web3: null,
-        }
-    }
-
-    instantiateContract() {
-        /*
-         * SMART CONTRACT EXAMPLE
-         *
-         * Normally these functions would be called in the context of a
-         * state management library, but for convenience I've placed them here.
-         */
-
-        const contract = require('truffle-contract')
-        const assetToken = contract(AssetToken)
-        const crowdSaleContract = contract(CrowdSale)
-        assetToken.setProvider(this.state.web3.currentProvider)
-        crowdSaleContract.setProvider(this.state.web3.currentProvider)
-
-        // Declaring this for later so we can chain functions on assetToken.
-        var assetTokenInstance
-        var crowdSaleInstance
-
-        // Get accounts.
-        this.state.web3.eth.getAccounts((error, accounts) => {
-            assetToken.deployed().then((instance) => {
-                assetTokenInstance = instance
-
-                // Stores a given value, 5 by default.
-                return assetTokenInstance.name.call();
-            }).then((result) => {
-                // Update state with the result.
-                this.setState({
-                    tokenName: result
-                });
-                return assetTokenInstance.totalSupply.call();
-            }).then((totalSupply) => {
-                this.setState({
-                    totalSupply: totalSupply.c[0],
-                    tokenAddress: assetTokenInstance.address
-                });
-                return crowdSaleContract.deployed();
-            }).then((inst) => {
-                crowdSaleInstance = inst;
-                this.setState({
-                    crowdSaleAddress: crowdSaleInstance.address
-                });
-                return crowdSaleInstance.tokenReward.call();
-            }).then((tokenReward) => {
-                this.setState({
-                    tokenReward: tokenReward
-                });
-            });
-        })
-    }
-
-    componentWillMount() {
-        // Get network provider and web3 instance.
-        // See utils/getWeb3 for more info.
-
-        getWeb3
-            .then(results => {
-                this.setState({
-                    web3: results.web3
-                })
-
-                // Instantiate contract once web3 provided.
-                this.instantiateContract()
-            })
-            .catch(() => {
-                console.log('Error finding web3.')
-            })
-    }
-
-
-    render() {
-        return <div>{this.state.totalSupply}</div>
-    }
-
-}
-
-class TokensSold extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            storageValue: 0,
-            web3: null,
-        }
-    }
-
-    render() {
-        return <div>{this.state.storageValue}</div>
-    }
-
-}
-
-
-class TokenPrice extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            storageValue: 0,
-            web3: null,
-        }
-    }
-
-    render() {
-        return <div>{this.state.storageValue}</div>
-    }
-
-}
 
 class App extends Component {
     constructor(props) {
@@ -229,12 +112,11 @@ class App extends Component {
         let torender = null
 
         if (this.state.submitted) {
-            torender = <SaleForm
-            handleBuy={this.handleBuy}
-            tokenName={this.state.tokenName}
+            torender = <SaleActionView
             totalSupply={this.state.totalSupply}
-            tokenReward={this.state.tokenReward}
-            crowdSaleAddress={this.state.crowdSaleAddress} />
+            handleBuy={this.handleBuy}
+            crowdSaleAddress={this.state.crowdSaleAddress}
+            />
         } else {
             torender = <SaleInfo
             handleSubmit={this.handleSubmit}
@@ -256,65 +138,59 @@ class App extends Component {
     }
 }
 
+
 class SaleInfo extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
-        return (<div>
-                 <table className="table">
-                    <tbody>
-                        <tr><th>Total released Number of tokens</th><td>{this.props.totalSupply}</td></tr>
-                        <tr><th>Token supply remaining</th><td>{this.props.totalSupply}</td></tr>
-
-
-                        <tr><th>Price per token</th><td>USD 1.50</td></tr>
-                        <tr><th>Sale duration</th><td>30 days</td></tr>
-                        <tr><th>Sale start date</th><td>Nov 15, 2017</td></tr>
-                                              
-                    </tbody>
-                 </table>
-                 
-                <button type="button" className='btn btn-primary' onClick={this.props.handleSubmit}>Get some tokens!</button>
-                </div>
-        )
+        return (
+            <div className="panel-body">
+                <h3 className="text-center">SunIntensity Token Sale</h3>
+                <div className="row">
+                <div className="col-sm-4 text-center">
+                <p><span id="tokensforsale" className="text-info"><div data-reactroot="">{this.props.totalSupply}</div></span><br/>
+                            SunIntensity tokens for sale</p>
+                <p></p></div>
+                <div className="col-sm-4 text-center">
+                <p><span id="tokenssold" className="text-info"><div data-reactroot="">0</div></span><br/>
+                            SunIntensity tokens sold</p>
+                <p></p></div>
+                <div className="col-sm-4 text-center">
+                <p>
+                            <span id="tokenprice" className="text-info"><div data-reactroot="">USD 1.50</div></span><br/>
+                                    Price per SunIntensity token</p>
+                <p></p></div></div>
+                <p>Sale ends at block <span className="text-info ">[NUMBERS]</span>, approximately at Nov 13th 12pm EST. <span class="text-info ">[NUMBERS]</span> blocks remaining.</p>
+                <div className="row ">
+                <p>        <button onClick={this.props.handleSubmit} className="btn btn-info btn-lg col-sm-4 col-sm-offset-4 ">Buy Tokens!</button></p></div></div>
+        );
     }
 }
 
-class SaleForm extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-
-
+class SaleActionView extends Component {
     render() {
-
-        return <div><h3>You can purchase GRID tokens here</h3> 
-
-                    <table className="table">
-                        <tbody>
-                            <tr><th>Token Name</th><td>{this.props.tokenName}</td></tr>
-                            <tr><th>Token Sale Contract Address</th>
-                            <td>
-                                <h4><a href={"https://etherscan.io/address/" + this.props.crowdSaleAddress}>
-                                <span id="crowdSaleAddress" className="badge badge-light">{this.props.crowdSaleAddress}</span>
-                                </a></h4> <label htmlFor="crowdSaleAddress  ">Click address to view on Etherscan</label>
-                            </td></tr>
-
-                            <tr><th>Tokens available</th><td>{this.props.totalSupply}</td></tr>
-                            
-                            
-                            <tr><th colSpan="2">
-                            <form onSubmit={this.props.handleBuy}>
-                                  <input type="submit" className='btn btn-primary' value="Go Back" />
-                            </form>
-                            </th></tr>                        
-                        </tbody>
-                     </table>
-                </div>
+        return (
+            <div className="panel-body">
+                <h3 className="text-center">SunIntensity Token Sale</h3>
+                <div className="row">
+                <div className="col-sm-4 text-center">
+                <p><span id="tokensforsale" className="text-info"><div data-reactroot="">{this.props.totalSupply}</div></span><br/>
+                            SunIntensity tokens for sale</p>
+                <p></p></div>
+                <div className="col-sm-4 text-center">
+                <p><span id="tokenssold" className="text-info"><div data-reactroot="">{this.props.crowdSaleAddress}</div></span><br/>
+                            SunIntensity CrowdSale Address</p>
+                <p></p></div>
+                <div className="col-sm-4 text-center">
+                <p>
+                            <span id="tokenprice" className="text-info"><div data-reactroot="">USD 1.50</div></span><br/>
+                                    Price per SunIntensity token</p>
+                <p></p></div></div>
+                <p>See instructions below on how to send tokens to this address</p>
+                <div className="row ">
+                <p>        <button onClick={this.props.handleBuy} className="btn btn-info btn-lg col-sm-4 col-sm-offset-4 ">Back to Token Info!</button></p></div></div>
+        );
     }
-
 }
 
-export { App, TokensForSale, TokensSold, TokenPrice }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App, TokensForSale, TokensSold, TokenPrice } from './App'
+import App from './App'
 
 
 ReactDOM.render(
-    <TokensForSale />,
-    document.getElementById('tokensforsale')
-);
-
-
-ReactDOM.render(
-    <TokensSold />,
-    document.getElementById('tokenssold')
-);
-
-
-ReactDOM.render(
-    <TokenPrice />,
-    document.getElementById('tokenprice')
+    <App />,
+    document.getElementById('app')
 );
